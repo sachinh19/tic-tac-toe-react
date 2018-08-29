996ce8ef428abe14191417ce3d8a4161f75d4811
Sachin Haldavanekar <sachinhaldavanekar@gmail.com>
2018-08-29 15:03:00 -0700

    Separated components into files

---
+++ src/components/Square.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Square = (props) => (
+    <button
+      className="square"
+      onClick={() => props.onClick()}
+    >
+      {props.value}
+    </button>
+);
